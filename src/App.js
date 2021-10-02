3ac022cb38c83fd02fed4420ccd4a6b2a684966b
sathvik <Onlinegithub123!>
2021-10-02 18:54:54 -0500

    Added async course fetch from URL

--- src/App.js
+++ src/App.js
@@ -1,33 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-const schedule = {
-  title: "CS Courses for 2021-2022",
-  courses: {
-    "F101" : {
-      "id" : "F101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "id" : "F110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "id" : "S313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "id" : "S314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
 
 const Banner = ({ title }) => (
   <h1> { title } </h1>
@@ -63,12 +37,28 @@ const CourseList = ({ courses }) => (
   
 );
 
+const App = () =>  {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
-const App = () =>  (
-  <div className="container">
+  
+  useEffect (() => {
+    const fetchScheule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchScheule();
+  }, []);
+  
+
+  if(!schedule) return <h1> Loading Schedule </h1>;
+
+  return (<div className="container">
     <Banner title = { schedule.title } />
     <CourseList courses = { schedule.courses }/> 
-  </div>
-);
+  </div>)
+}
 
 export default App;
